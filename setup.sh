dc73a774ef9d6ef09489e91f29abb7090b63ae81
Nantor <4553079+Nantor@users.noreply.github.com>
2020-02-09 17:12:36 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ PRODUCT_ID=1000
 
 # install dependencies
 apt update
-apt install python3-pip libhidapi-hidraw0
+apt install -y python3-pip libhidapi-hidraw0
 pip3 install hid keyboard
 
 # detect Buzz controller
